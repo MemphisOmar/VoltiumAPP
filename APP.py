51ec6f3337eb404518153d875798bde0526580de
Edgar Omar Omar Martínez Vega <101699281+MemphisOmar@users.noreply.github.com>
2024-11-26 22:42:48 -0600

    MENUV2
    MENU CON COLORES

--- APP.py
+++ APP.py
@@ -1,10 +1,18 @@
 import flet as ft
 
+from flet import (
+    Page,
+    colors
+)
+
+
 def main(page: ft.Page):
     page.title = "VOLTIUM"
+    page.bgcolor = colors.PURPLE_500
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.background_color = ft.colors.LIME_ACCENT  # Cambia el color de fondo a azul claro
+    page.window_width = 800  # Ancho de la ventana
+    page.window_height = 900  # Alto de la ventana
 
     def jugar_click(e):
         page.dialog = ft.AlertDialog(title=ft.Text("¡Comenzando el juego!"))
@@ -19,19 +27,24 @@ def main(page: ft.Page):
     def salir_click(e):
         page.window_close()
 
-    jugar_button = ft.ElevatedButton(text="JUGAR", on_click=jugar_click, width=200, height=50)
+    jugar_button = ft.ElevatedButton(text="JUGAR", on_click=jugar_click, width=200, height=50, color=colors.LIGHT_GREEN_ACCENT_700)
     ayuda_button = ft.ElevatedButton(text="AYUDA", on_click=ayuda_click, width=200, height=50)
     salir_button = ft.ElevatedButton(text="SALIR", on_click=salir_click, width=200, height=50)
 
     page.add(
-        ft.Column(
-            [
-                jugar_button,
-                ayuda_button,
-                salir_button
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        ft.Container(
+            content=ft.Column(
+                [
+                    jugar_button,
+                    ayuda_button,
+                    salir_button
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+            image_src="R.png",  # Ruta de la imagen de fondo
+            expand=True,
+            alignment=ft.alignment.center,
         )
     )
 
