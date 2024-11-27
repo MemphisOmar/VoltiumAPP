a39b04a043572967cb37dbd021e68aab22266ef1
Edgar Omar Omar Martínez Vega <101699281+MemphisOmar@users.noreply.github.com>
2024-11-26 21:52:58 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,9 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Menú del Videojuego"
+    page.title = "VOLTIUM"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.background_color = ft.colors.LIME_ACCENT  # Cambia el color de fondo a azul claro
 
     def jugar_click(e):
         page.dialog = ft.AlertDialog(title=ft.Text("¡Comenzando el juego!"))
@@ -15,14 +16,19 @@ def main(page: ft.Page):
         page.dialog.open = True
         page.update()
 
-    jugar_button = ft.ElevatedButton(text="JUGAR", on_click=jugar_click)
-    ayuda_button = ft.ElevatedButton(text="AYUDA", on_click=ayuda_click)
+    def salir_click(e):
+        page.window_close()
+
+    jugar_button = ft.ElevatedButton(text="JUGAR", on_click=jugar_click, width=200, height=50)
+    ayuda_button = ft.ElevatedButton(text="AYUDA", on_click=ayuda_click, width=200, height=50)
+    salir_button = ft.ElevatedButton(text="SALIR", on_click=salir_click, width=200, height=50)
 
     page.add(
         ft.Column(
             [
                 jugar_button,
-                ayuda_button
+                ayuda_button,
+                salir_button
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
