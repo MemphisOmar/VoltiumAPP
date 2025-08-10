ee3c3931623accddef6d55a8e0316c51c9cf23ee
Edgar Omar Omar Martínez Vega <101699281+MemphisOmar@users.noreply.github.com>
2025-08-10 16:23:52 -0600

    Optimizacion de ayuda y logo

--- MAIN/ayuda.py
+++ MAIN/ayuda.py
@@ -9,7 +9,7 @@ def mostrar_ayuda(page: ft.Page):
 
     def mostrar_codigo_colores(e):
         contenido_ayuda.content = ft.Column([
-            ft.Image(src="MAIN/Resistencia.png", width=400, height=400, fit=ft.ImageFit.CONTAIN),
+            ft.Image(src="MAIN/Resistencia.png", width=320, height=320, fit=ft.ImageFit.CONTAIN),
             ft.ElevatedButton("Regresar", on_click=mostrar_menu_ayuda)
         ], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
         page.update()
@@ -19,7 +19,7 @@ def mostrar_ayuda(page: ft.Page):
         
     def mostrar_menu_ayuda(e=None):
         contenido_ayuda.content = ft.Column([
-            ft.Text("¿Qué tipo de ayuda necesita?", size=20, weight=ft.FontWeight.BOLD),
+            ft.Text("¿Qué tipo de ayuda necesita?", size=20, weight=ft.FontWeight.BOLD, color="black"),
             ft.ElevatedButton(
                 "Código de Colores",
                 on_click=mostrar_codigo_colores,
@@ -49,10 +49,12 @@ def mostrar_ayuda(page: ft.Page):
 
     contenido_ayuda = ft.Container(
         content=None,
-        padding=20,
-        width=500,
-        bgcolor="#88B98A",
-        border_radius=10
+        padding=30,
+        width=380,
+        height=420,
+        bgcolor="#88B98ACC",  # Verde con menos transparencia
+        border_radius=30,
+        alignment=ft.alignment.center
     )
 
     overlay_ayuda = ft.Container(
@@ -60,12 +62,12 @@ def mostrar_ayuda(page: ft.Page):
             ft.Container(
                 content=contenido_ayuda,
                 alignment=ft.alignment.center,
-                bgcolor="#88B98A",
-                border_radius=10,
-                padding=10,
+                bgcolor=None,
+                border_radius=30,
+                padding=0,
             )
         ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER),
-        bgcolor="#88B98A99",  # Semitransparente
+        bgcolor="#00000033",  # Fondo overlay muy transparente
         alignment=ft.alignment.center,
         expand=True
     )
